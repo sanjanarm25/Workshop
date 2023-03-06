97bac67780042ef28515073d7504f7a6d7f72873
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-06 12:42:38 +0530

    concatination of two strings added

---
+++ src/com/bridgelabz/StringConcatination.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.Arrays;
+
+public class StringConcatination {
+    public static void main(String[] args) {
+        String str1="Hello ";
+        String str2="Welcome ";
+        String output=str1+str2;
+        System.out.println(str1+" "+str2);
+
+    }
+}
